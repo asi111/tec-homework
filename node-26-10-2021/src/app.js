b001dfa0d81846f892eb907f19ccc532f191c22b
asi111 <asrasfarada@gmail>
2021-10-27 18:36:01 +0300

    push

--- node-26-10-2021/src/app.js
+++ node-26-10-2021/src/app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const fs = require("fs")
-const axios = require("axios")
-const path = require("path")
+
+
 
 const app = express();
 const PORT = 8080;
-
-const publicPath = path.join(__dirname,"..","public")
 
 let comments = "comments.json"
 
